6b55d7aa0636a7a9ac2ed45cbde431d37d5c7e4b
seancour <seancour@buffalo.edu>
2024-11-20 10:38:32 -0500

    EKF visualizer not working yet but heres progress.

--- Unity/Excavator/Assets/Scripts/EKF_Visualizer.cs
+++ Unity/Excavator/Assets/Scripts/EKF_Visualizer.cs
@@ -4,27 +4,28 @@ using OdometryMsg = RosMessageTypes.Nav.OdometryMsg;
 
 public class EKF_Visualizer : MonoBehaviour
 {
-    private GameObject redCube;
+    public Mesh arrowMesh; // Assign the imported Arrow mesh in the inspector
+    private Vector3 initialPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ROSConnection.GetOrCreateInstance().Subscribe<OdometryMsg>("joy_deltacan", OnOdomestryMsgRecieved);
-        // Create a red cube
-        GameObject redCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        redCube.transform.localScale = new Vector3(1, 1, 1);
-        redCube.GetComponent<Renderer>().material.color = Color.red;
+        initialPosition = transform.position; // Initialize the initial position of the mesh
+        ROSConnection.GetOrCreateInstance().Subscribe<OdometryMsg>("/Odometry/filtered", OnOdomestryMsgRecieved);
     }
 
     void OnOdomestryMsgRecieved(OdometryMsg commandMessage)
     {
         Debug.Log(commandMessage);
-        // Update the position of the red cube
-        Vector3 newPosition = new(
+        // Calculate the new position based on the initial position and the received message
+        Vector3 newPosition = initialPosition + new Vector3(
             (float)commandMessage.pose.pose.position.x,
             (float)commandMessage.pose.pose.position.y,
             (float)commandMessage.pose.pose.position.z
         );
-        redCube.transform.position = newPosition;
+
+        // Update the position of the mesh
+        transform.position = newPosition;
     }
 
     // Update is called once per frame
